fce29b15f131769bfd206997f7bcb9ad11f53f9f
Mykolai <1kolia1@ukr.net>
2019-05-08 19:51:12 +0300

    ewqsz

--- src/main/java/Bot/Bot.java
+++ src/main/java/Bot/Bot.java
@@ -5,6 +5,7 @@ import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.api.methods.GetFile;
+import org.telegram.telegrambots.meta.api.methods.polls.SendPoll;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
 import org.telegram.telegrambots.meta.api.objects.InputFile;
@@ -37,11 +38,11 @@ import java.util.List;
 import static org.apache.commons.io.FileUtils.getFile;
 
 public class Bot extends TelegramLongPollingBot {
-     static long chatid=516538254;
+     static long chatid=314254027;
     static String info_for_zakaz="";
     List<Sticker> stickers = new ArrayList<>();
     InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
-    static int number;
+    static int number=1;
     public  ReplyKeyboardMarkup replyKeyboardMarkup=new ReplyKeyboardMarkup();
     public static void main(String... args) {
         System.out.println("start");
@@ -71,53 +72,71 @@ public class Bot extends TelegramLongPollingBot {
             sendMessage.setChatId(message.getChatId());
             System.out.println(message.getChatId());
             replyKeyboardMarkup.setResizeKeyboard(true);
-            replyKeyboardMarkup.setOneTimeKeyboard(true);
+            replyKeyboardMarkup.setOneTimeKeyboard(false);
             replyKeyboardMarkup.setSelective(true);
-            if(message.getText().equals("О боте:")){
-                try {
-
-                    sendApiMethod(sendMessage.setText("Этот бот создает макет Sticker Boom из вашых стикеров"));
-                } catch (TelegramApiException e) {
-                    e.printStackTrace();
-                }
-            }
-            if(message.getText().equals("Оформить:")){
-                sendMessage.setChatId(chatid);
-                sendMessage.setText("Заказ про то шо коля хуй)");
-                try {
-                    sendApiMethod(sendMessage);
-                } catch (TelegramApiException e) {
-                    e.printStackTrace();
-                }
-            }
-            if(message.getText().equals("Сделать макет:")){
-                try {
-
-                    sendApiMethod(sendMessage.setText("Отправте 12 стикеров"));
-                } catch (TelegramApiException e) {
-                    e.printStackTrace();
-                }
-            }
-
-
-            else
+            if(replyKeyboardMarkup.getKeyboard().size()==1)
             {
-                sendMessage.setText("Функнции");
+                sendMessage.setText("Заказ оформлен, ожидайте вскоре с вами свяжуться");
                 try {
-
-                    sendApiMethod(sendMessage.setReplyMarkup(remakeButtons(message.getText(),replyKeyboardMarkup,number)));
+                    sendApiMethod(sendMessage.setReplyMarkup(remakeButtons("Оформить", replyKeyboardMarkup, number)));
                 } catch (TelegramApiException e) {
                     e.printStackTrace();
                 }
 
             }
+            switch (message.getText()) {
+                   case   "О боте:" :
+                      try {
 
-            if (message.getText().indexOf("вул")>0)
-            {
-               sendMessage.setChatId(chatid);
-               sendMessage.setText(message.getText());
+                        sendApiMethod(sendMessage.setText("Этот бот создает макет Sticker Book из вашых стикеров").setReplyMarkup(remakeButtons(message.getText(), replyKeyboardMarkup, number)));
+                    } catch (TelegramApiException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+
+                    case  "Оформить:"  :
+                         sendMessage.setChatId(chatid);
+
+                         sendMessage.setText("Заказ про то шо коля хуй)");
+
+                         try {
+                             sendApiMethod(sendMessage);
+                         } catch (TelegramApiException e) {
+                             e.printStackTrace();
+                         }
+                       break;
+                      case  "Сделать макет:"  :
+                       try{
+
+
+                           sendApiMethod(sendMessage.setText("Отправте 12 стикеров").setReplyMarkup(remakeButtons(message.getText(), replyKeyboardMarkup, number)));
+                           } catch (TelegramApiException e) {
+                           e.printStackTrace();
+                           }
+                        break;
+                        default:
+                          try {
+                              if (message.getText().length()>20)
+                              {
+                                  sendMessage.setChatId(chatid);
+                                  sendMessage.setText(message.getText());
+                                  execute(sendMessage);
+                                  sendMessage.setChatId(message.getChatId());
+                                  sendMessage.setText("Ваш заказ принят\n ожидайте с вами свяжуться\n не желаете попробовать ещё?");
+                                  sendApiMethod(sendMessage.setReplyMarkup(remakeButtons("/yes/no", replyKeyboardMarkup, number)));
+
+                              }
+                              else {
+                                  sendMessage.setText("----");
+                                  sendApiMethod(sendMessage.setReplyMarkup(remakeButtons(message.getText(), replyKeyboardMarkup, number)));
+                              }
+                          } catch (TelegramApiException e) {
+                                 e.printStackTrace();
+                          }
+                          break;
 
             }
+
         }
         if(message!=null && message.hasSticker())
         {
@@ -125,6 +144,14 @@ public class Bot extends TelegramLongPollingBot {
             System.out.println(message.getChatId());
             if (stickers.size()!=12){
                 stickers.add(message.getSticker());
+                SendMessage sendMessage=new SendMessage();
+                sendMessage.setText("next");
+                sendMessage.setChatId(message.getChatId());
+                try {
+                    sendApiMethod(sendMessage.setReplyMarkup(remakeButtons("Сделать макет:", replyKeyboardMarkup, number)));
+                } catch (TelegramApiException e) {
+                    e.printStackTrace();
+                }
 
                 number++;
             try {
@@ -144,8 +171,15 @@ public class Bot extends TelegramLongPollingBot {
             {
                 try {
                     SendPhoto send=   combineALLImages(message,"src/main/resources/", 12);
+                    SendMessage sendMessage=new SendMessage();
+                    sendMessage.setChatId(message.getChatId());
+                    sendMessage.setText("Теперь давайте оформим заказ");
+                    sendApiMethod(sendMessage.setReplyMarkup(remakeButtons("/all", replyKeyboardMarkup, number)));
+                execute(send);
+                Thread.sleep(100);
+                    sendMessage.setText("Напишите свое Ф.И.О.\nсвой город\n  и отделение новой почты ");
+                    sendApiMethod(sendMessage.setReplyMarkup(remakeButtons("/all", replyKeyboardMarkup, number)));
 
-                    execute(send);
                     number=0;
 
 
@@ -188,6 +222,7 @@ public class Bot extends TelegramLongPollingBot {
             e.printStackTrace();
         }
     }
+
     public synchronized void sendMsg(String chatId, String s) {
         SendMessage sendMessage = new SendMessage();
         sendMessage.setReplyMarkup(remakeButtons(sendMessage.getText(),replyKeyboardMarkup,number));
@@ -202,9 +237,6 @@ public class Bot extends TelegramLongPollingBot {
         }
     }
 
-
-
-
     public synchronized ReplyKeyboardMarkup remakeButtons(String s, ReplyKeyboardMarkup replyKeyboardMarkup,int n) {
         KeyboardRow keyboardRow1=new KeyboardRow();
         KeyboardButton keyboardButton1=new KeyboardButton();
@@ -217,29 +249,50 @@ public class Bot extends TelegramLongPollingBot {
         klava.add(keyboardRow1);
         replyKeyboardMarkup.setKeyboard(klava);
         switch (s){
+            case "да":
+                return replyKeyboardMarkup;
+            case "нет":
+                return replyKeyboardMarkup;
             case "/start":
                 return replyKeyboardMarkup;
-            case "О боте:":
+
+                case "О боте:":
                 keyboardRow1.clear();keyboardRow1.add(keyboardButton1.setText("Сделать макет:"));
-            case "Сделать макет:":
+
+                    return replyKeyboardMarkup;
+            case"/all":
+                keyboardRow1.clear();
+                klava.add(keyboardRow1);
+                    replyKeyboardMarkup.setKeyboard(klava);
+                return replyKeyboardMarkup;
+            case "/yes/no":
+                keyboardRow1.clear();
+                keyboardButton1.setText("да");
+                keyboardButton1.setText("нет");
+
+                return replyKeyboardMarkup;
+                case "Сделать макет:":keyboardRow1.clear();
+
+
                 keyboardRow1.add(keyboardButton2.setText("Осталось "+n+"/12"));
-                keyboardRow1.add(keyboardButton1.setText("Оформить"));
+                    return replyKeyboardMarkup;
             case "Оформить:":keyboardRow1.clear();
                 keyboardButton1.setText("О боте:");
                 keyboardButton2.setText("Сделать макет:");
                 keyboardRow1.add(keyboardButton1);
                 keyboardRow1.add(keyboardButton2);
-
+                return replyKeyboardMarkup;
+                default:
+                    return replyKeyboardMarkup;
 
         }
 
 
 
-        return replyKeyboardMarkup;
+
 
 
     }
-
 
     public void DecodedWebP(int number) throws IOException {
         String inputWebpPath = "test_pic/sticker"+number+".webp";
@@ -343,7 +396,7 @@ public class Bot extends TelegramLongPollingBot {
         ImageIO.write(finalImg, "png", final_Image);
 
 
-        return new SendPhoto().setChatId(chatid).setPhoto(new File("src/main/resources/stickerpack.png")).setCaption(info_for_zakaz);
+        return new SendPhoto().setChatId(message.getChatId()).setPhoto(new File("src/main/resources/stickerpack.png")).setCaption(info_for_zakaz);
     }
 
     @Override
